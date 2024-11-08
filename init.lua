631840e475762c617da4de7a08c19ff4a8d68fac
Andrew Callahan <andrew.callahan@velocigo.com>
2024-11-08 09:32:45 -0600

    Force line numbering

--- init.lua
+++ init.lua
@@ -9,4 +9,6 @@ vim.opt.termguicolors = true
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.softtabstop = 4 -- Seriously, all tabs need to be 4 ppl
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.opt.number = true -- Set line numbering to absolute
+vim.opt.relativenumber = false -- Force relative numbering off
 --vim.o.loaded_perl_provider = 0
