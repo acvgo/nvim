34b601fbff15bdcc52ab946839ea1aa06d76c193
Andrew Callahan <andrew.callahan@velocigo.com>
2025-05-08 16:59:52 -0500

    v1.x pin until LazyVim updates handling of lspconfig mappings

---
+++ lua/plugins/mason-workaround.lua
@@ -0,0 +1,5 @@
+-- This file is for pinning Mason version to v1.x until breaking changes are incorporated into lazyvim/neovim
+return {
+	{ "mason-org/mason.nvim", version = "^1.0.0" },
+	{ "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
