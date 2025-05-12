a5c7f0a4466abfb90c9d13d3709c468bcdfe5480
Andrew Callahan <andrew.callahan@velocigo.com>
2025-05-12 09:23:20 -0500

    Never ver of Lazyvim pins the mason version at 1.x internally
    Commented out and moved the workaround file to unused.
    It can serve as an example/howto for the future.

--- lua/plugins/mason-workaround.lua
+++
@@ -1,5 +0,0 @@
--- This file is for pinning Mason version to v1.x until breaking changes are incorporated into lazyvim/neovim
-return {
-	{ "mason-org/mason.nvim", version = "^1.0.0" },
-	{ "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-}
